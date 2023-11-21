48365d1e40580786b9a086b72c035af9f5ed2d25
ziminliu <liuziminliuzimin@gmail.com>
2023-11-21 21:24:39 +0800

    ci feat: change host

--- libraries/config.js
+++ libraries/config.js
@@ -2,7 +2,7 @@ export const config = {
 	endpoint: "https://k450jq.logto.app/",
 	appId: "z2xhsckmeehoeaor0rx1u",
 	appSecret: "sX6Eh71mqKUbcuAO96YWva6DSWAFOLmg",
-	baseUrl: "http://www.zimin.fun", // Change to your own base URL
+	baseUrl: "http://gpt.zimin.fun", // Change to your own base URL
 	cookieSecret: "6bocIuvpC5ChgROL1DNHRiNvKlySJEAl", // Auto-generated 32 digit secret
 	cookieSecure: process.env.NODE_ENV === "production",
 };
